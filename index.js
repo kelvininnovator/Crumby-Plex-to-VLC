9762d40d21993681e6dcd12e2d27f829ef06fb3b
kelvininnovator <kelvin.innovator@gmail.com>
2016-02-15 06:01:22 +1030

    remove fluff

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var buttons = require('sdk/ui/button/action');
 var tabs = require("sdk/tabs");
 
-
-
 const { Cc, Ci, Cu } = require('chrome');
 
 
@@ -19,22 +17,13 @@ var button = buttons.ActionButton({
 
 
 
-
-
 function handleClick(state) {
 
 
 //Linux Checkbox
 var isLinux = require("sdk/simple-prefs").prefs.crumblyLinuxOption;
-console.log("Linux options is: "+isLinux);
-
-
-
-
-
 
 	var mainVideoFile ="";
-
 	var tab = require("sdk/tabs").activeTab.attach({
   contentScript: "self.postMessage(document.body.querySelector('.download-btn.secondary-only').href);",
   
@@ -42,26 +31,15 @@ console.log("Linux options is: "+isLinux);
   {
   
 	mainVideoFile=data+"";
-    console.log("Tab data received: " + data);
-		console.log("Main video file at selector find: " + mainVideoFile)
 		
 			var exeFile = Cc["@mozilla.org/file/local;1"].createInstance(Ci.nsILocalFile);
-			
-		
 
 		if (isLinux==true){
-			console.log("True up in here");
 			exeFile.initWithPath("/usr/bin/vlc");
 			
 			}	else {
-			console.log("False up in here");
 			exeFile.initWithPath("C:\\Program Files (x86)\\VideoLAN\\VLC\\vlc.exe");
 			}
-			
-	//exeFile.initWithPath("C:\\Program Files (x86)\\VideoLAN\\VLC\\vlc.exe");
-	//var parameter="http://192.168.0.20:32400/library/parts/3471/file.avi?stripTags=0";
-	
-	console.log("Main video file at VLC: " + mainVideoFile);
 	
 	if(exeFile.exists()){
 		var process = Cc["@mozilla.org/process/util;1"].createInstance(Ci.nsIProcess);  
